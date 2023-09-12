ac9d1f58b14e24888d652443e9ffd99ea39c8e9a
TessTang <z8937200@gmail.com>
2023-09-13 01:27:24 +0800

    0913更新v3

--- router/blog.js
+++ router/blog.js
@@ -4,7 +4,7 @@ var blog = express.Router();
 var db = require('../db');
 
 
-
+//分頁
 blog.get('/page/:page([0-9]+)', function(req, res){
     var myPage = req.params.page;
     if (myPage <= 0) {
@@ -47,17 +47,11 @@ blog.get('/page/:page([0-9]+)', function(req, res){
    })
 });
 
-
-// blog.get("/", function(req, res){
-//     var sql1 = "select * from article;"
-
-//     db.query(sql1, function(err, data){
-//         res.render('blog', {
-//             data: data
-//         });
-//     })
 })
 
+
+
+//tag
 blog.get('/tags', function(req, res){
     var sql1 = "select * from tag;"
     db.query(sql1, function(err, tags){
@@ -67,30 +61,60 @@ blog.get('/tags', function(req, res){
         } else {
             console.log('資料抓成功');
             res.json(tags);
-
         }
     })
 })
 
+//待確認tag排序
+blog.get('/tag/:id', function(req, res){
+    var art_id2 = req.params.id;
+    var sql3 = "SELECT * FROM tag, atcxtag, article WHERE tag.tagid = atcxtag.tagid AND atcxtag.atcid = article.atcid AND tag.tagid = ?;"
+    var data = [req.params.id];
+    db.query(sql3, data, function(err, tags){
+        if(err){
+            console.log('tagid沒抓成功');
+            console.log(err);
+        } else {
+            console.log('tagid抓成功');
+            res.json(tags);
+        }
+    })
+})
 
 
 blog.get('/inside/:id', function(req, res){
     var art_id = req.params.id;
-    var sql2 = "select * from article where atcid = ?;"
-    var data = [req.params.id];
-    db.query(sql2, data, function(err, art){
+    var sql2 = "SELECT * FROM article WHERE atcid = ?;";
+    var sql4 = "SELECT * FROM article,atcxtag,tag WHERE article.atcid = atcxtag.atcid AND atcxtag.tagid = tag.tagid AND article.atcid = ?;";
+    
+    // 第一次查詢
+    db.query(sql2, [art_id], function(err, result1){
         if(err){
             console.log('抓文章失敗');
             console.log(err);
         } else {
-            console.log(art);
-            res.render('blog_inside',{
-                data: art
-            })
+            // 第二次查詢
+            db.query(sql4, [art_id], function(err, result2){
+                if(err){
+                    console.log('抓取文章標籤失敗');
+                    console.log(err);
+                } else {
+                    console.log(result1);
+                    console.log(result2);
+                    res.render('blog_inside',{
+                        data: {
+                            article: result1,
+                            tags: result2
+                        },
+                    });
+                }
+            });
         }
-    })
-    // console.log(art_id);
-    // res.render('blog_inside');
-})
+    });
+});
+
+
+
+
 
 module.exports = blog;
